263ed15795118fd27431cfea8061889faad7efde
Ujjwal Sinha <157524726+ujjwal2131@users.noreply.github.com>
2024-09-11 08:37:25 +0530

    Update Request 04.sql

--- SQL Queries/Request 04.sql
+++ SQL Queries/Request 04.sql
@@ -12,24 +12,24 @@ WITH unique_products_2020 AS (
 		p.segment,
 		COUNT(DISTINCT s.product_code) AS product_count_2020
 	FROM  fact_sales_monthly s
-    JOIN dim_product p USING(product_code)
-    WHERE fiscal_year = 2020
-    GROUP BY segment
+    	JOIN dim_product p USING(product_code)
+    	WHERE fiscal_year = 2020
+    	GROUP BY segment
 ),
 unique_products_2021 AS (
 	SELECT
 		p.segment,
 		COUNT(DISTINCT s.product_code) AS product_count_2021
 	FROM  fact_sales_monthly s
-    JOIN dim_product p USING(product_code)
-    WHERE fiscal_year = 2021
-    GROUP BY segment
+    	JOIN dim_product p USING(product_code)
+    	WHERE fiscal_year = 2021
+    	GROUP BY segment
 )
 SELECT 
 	p20.segment,
-    p20.product_count_2020,
-    p21.product_count_2021,
-    (product_count_2021 - product_count_2020) AS difference
+    	p20.product_count_2020,
+    	p21.product_count_2021,
+    	(product_count_2021 - product_count_2020) AS difference
 FROM unique_products_2020 p20
 JOIN unique_products_2021 p21 USING(segment)
 ORDER BY difference DESC;
