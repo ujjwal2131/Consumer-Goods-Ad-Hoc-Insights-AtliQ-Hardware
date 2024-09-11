81aea414fda576e56b3056e109a503283bd4c912
Ujjwal Sinha <157524726+ujjwal2131@users.noreply.github.com>
2024-09-11 08:38:34 +0530

    Update Request 06.sql

--- SQL Queries/Request 06.sql
+++ SQL Queries/Request 06.sql
@@ -8,14 +8,14 @@ The final output contains these fields,
 */
 
 SELECT
-    c.customer_code,
-    c.customer,
-    ROUND(AVG(p.pre_invoice_discount_pct)*100,2) AS average_discount_percentage
+    	c.customer_code,
+    	c.customer,
+    	ROUND(AVG(p.pre_invoice_discount_pct)*100,2) AS average_discount_percentage
 FROM dim_customer c
 JOIN fact_pre_invoice_deductions p USING(customer_code)
 WHERE 
 	p.fiscal_year = 2021
-    AND c.market = 'India'
+    	AND c.market = 'India'
 GROUP BY c.customer_code    
 ORDER BY average_discount_percentage DESC
 LIMIT 5;
